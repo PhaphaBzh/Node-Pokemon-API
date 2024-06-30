fd4248605de071b78e12b18a8fa1ff89881e12fd
steph <stephanie.moissonnier@gmail.com>
2024-06-30 18:23:11 +0200

    Gestion des erreurs

--- app.js
+++ app.js
@@ -23,4 +23,10 @@ require('./src/routes/createPokemon')(app)
 require('./src/routes/updatePokemon')(app)
 require('./src/routes/deletePokemon')(app)
 
+//On ajoute la gestion des erreurs 404
+app.use(({ res }) => {
+    const message = 'Impossible de trouver la ressource demandée. Vous pouvez essayer une autre URL.'
+    res.status(404).json({ message })
+})
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -3,6 +3,10 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
     app.delete('/api/pokemons/:id', (req, res) => {
         Pokemon.findByPk(req.params.id).then(pokemon => {
+            if (pokemon === null) {
+                const message = 'Le pokémon demandé n\'existe pas. Réessayer avec un autre identifiant'
+                return res.status(404).json(message)
+            }
             const pokemonDeleted = pokemon;
             Pokemon.destroy({
                 where: { id: pokemon.id }
@@ -12,5 +16,9 @@ module.exports = (app) => {
                     res.json({ message, data: pokemonDeleted })
                 })
         })
+            .catch(error => {
+                const message = "Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants."
+                res.status(500).json({ message, data: error })
+            })
     })
 }
